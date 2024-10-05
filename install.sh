5a5e4d796514f22f1e9873c3a61c2eb63d0276bd
Givari Hertz <hertzvibe001@gmail.com>
2024-10-05 10:31:09 +0700

    new subdomais enum tool

--- install.sh
+++ install.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
 
-# Install Golang
-echo "Installing Golang..."
+echo -e "\e[94m[+] Updating package list...\e[0m"
 sudo apt update
-sudo apt install -y golang
+
+# Install basic dependencies
+echo -e "\e[94m[+] Installing basic dependencies...\e[0m"
+sudo apt install -y curl git jq python3 python3-pip
 
 # Install subfinder
-echo "Installing subfinder..."
+echo -e "\e[94m[+] Installing subfinder...\e[0m"
 GO111MODULE=on go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-sudo cp $HOME/go/bin/subfinder /usr/local/bin/
+export PATH=$PATH:$(go env GOPATH)/bin
 
 # Install assetfinder
-echo "Installing assetfinder..."
-GO111MODULE=on go install github.com/tomnomnom/assetfinder@latest
-sudo cp $HOME/go/bin/assetfinder /usr/local/bin/
+echo -e "\e[94m[+] Installing assetfinder...\e[0m"
+go install github.com/tomnomnom/assetfinder@latest
+export PATH=$PATH:$(go env GOPATH)/bin
+
+# Install github-subdomains
+echo -e "\e[94m[+] Installing github-subdomains...\e[0m"
+git clone https://github.com/gwen001/github-subdomains
+cd github-subdomains
+pip3 install -r requirements.txt --break-system-packages
+cd ..
 
 # Install httpx
-echo "Installing httpx..."
+echo -e "\e[94m[+] Installing httpx...\e[0m"
 GO111MODULE=on go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-sudo cp $HOME/go/bin/httpx /usr/local/bin/
+export PATH=$PATH:$(go env GOPATH)/bin
 
-# Set permissions and symbolic link for subs_h3rtz.sh
-echo "Setting up h3rtz..."
-sudo cp subs-h3rtz.sh /usr/local/bin/h3rtz
-sudo chmod +x /usr/local/bin/h3rtz
+# Install dnsx
+echo -e "\e[94m[+] Installing dnsx...\e[0m"
+GO111MODULE=on go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+export PATH=$PATH:$(go env GOPATH)/bin
 
-echo "Installation complete."
+# Install certspotter
+echo -e "\e[94m[+] Installing certspotter...\e[0m"
+go install github.com/SSLMate/certspotter@latest
+export PATH=$PATH:$(go env GOPATH)/bin
+
+# Install Golang if not already installed (for subfinder, shuffledns, httpx, dnsx)
+if ! command -v go &> /dev/null
+then
+    echo -e "\e[94m[+] Installing Golang...\e[0m"
+    sudo apt install -y golang-go
+fi
+
+# Export Go binaries to PATH in .bashrc
+if ! grep -q 'export PATH=$PATH:$(go env GOPATH)/bin' ~/.bashrc; then
+    echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+echo -e "\e[92m[+] Installation complete! All tools are installed.\e[0m"
--- subs-h3rtz.sh
+++ subs-h3rtz.sh
@@ -2,41 +2,67 @@
 
 # Define file names
 DOMAINS_FILE="domains"
+GIT_TOKEN_FILE="$HOME/.git/git-token.txt"
 
+# Read GitHub token from file
+GITHUB_TOKEN=$(cat $GIT_TOKEN_FILE)
 
-"""
+# Print ASCII Art
+echo -e "
           ______   _______ _________ _______ 
 |\     /|/ ___  \ (  ____ )\__   __// ___   )
 | )   ( |\/   \  \| (    )|   ) (   \/   )  |
 | (___) |   ___) /| (____)|   | |       /   )
 |  ___  |  (___ ( |     __)   | |      /   / 
 | (   ) |      ) \| (\ (      | |     /   /  
-| )   ( |/\___/  /| ) \ \__   | |    /   (_/\
+| )   ( |/\___/  /| ) \ \__   | |    /   (_/\\
 |/     \|\______/ |/   \__/   )_(   (_______/
 
-	instagram: @givarirmdn - Happy Hacking ^_^
-"""
+\tinstagram: @givarirmdn - Happy Hacking ^_^
+"
+
 # Run subdomain enumeration tools
-echo -e "\e[94m[+] Running tool: subfinder...\e[0m"
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running tool: subfinder...\e[0m"
 subfinder -dL $DOMAINS_FILE | tee subs_subfinder.txt
 
-echo -e "\e[94m[+] Running tool: assetfinder...\e[0m"
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running tool: assetfinder...\e[0m"
 cat $DOMAINS_FILE | assetfinder -subs-only | tee subs_assetfinder.txt
 
+# Scrape GitHub for subdomains
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running tool: github-subdomains...\e[0m"
+while read DOMAIN; do
+  github-subdomains -d $DOMAIN -t $GITHUB_TOKEN -o subs_github.txt
+done < $DOMAINS_FILE
+
+# Use crt.sh to get subdomains from Certificate Transparency Logs
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running crt.sh...\e[0m"
+for DOMAIN in $(cat $DOMAINS_FILE); do
+  curl -s "https://crt.sh/?q=%25.$DOMAIN&output=json" | jq -r '.[].name_value' | sort -u | tee -a subs_crtsh.txt
+done
+
+# Use CertSpotter to gather more subdomains
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running CertSpotter...\e[0m"
+for DOMAIN in $(cat $DOMAINS_FILE); do
+  certspotter $DOMAIN | jq -r '.dns_names[]' | sort -u | tee -a subs_certspotter.txt
+done
 
 # Combine results from different tools
-echo -e "\e[94m[+] Combining subdomains from various sources...\e[0m"
-cat subs_subfinder.txt subs_assetfinder.txt | sort -u > allSubs.txt
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Combining subdomains from various sources...\e[0m"
+cat subs_subfinder.txt subs_assetfinder.txt subs_github.txt subs_crtsh.txt subs_certspotter.txt | sort -u > allSubs.txt
+
+# Run dnsx to filter out non-resolving subdomains
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running tool: dnsx...\e[0m"
+cat allSubs.txt | dnsx -silent | tee resolved_subs.txt
 
 # Run HTTPX with concurrency and categorize subdomains
-echo -e "\e[94m[+] Running tool: httpx\e[0m"
-httpx -l allSubs.txt -mc 200 | tee active_subs.txt
-httpx -l allSubs.txt -mc 301,302,303 | tee redirect_subs.txt
-httpx -l allSubs.txt -mc 401,403,405 | tee forbidden_subs.txt
-httpx -l allSubs.txt -mc 404,500 | tee takeovers_subs.txt
+echo -e "\e[94m[$(date '+%Y-%m-%d %H:%M:%S')] [+] Running tool: httpx...\e[0m"
+httpx -l resolved_subs.txt -mc 200 -threads 100 | tee active_subs.txt
+httpx -l resolved_subs.txt -mc 301,302,303 -threads 100 | tee redirect_subs.txt
+httpx -l resolved_subs.txt -mc 401,403,405 -threads 100 | tee forbidden_subs.txt
+httpx -l resolved_subs.txt -mc 404,500 -threads 100 | tee takeovers_subs.txt
 
-# Output colorful
+# Output results with colors
 echo -e "\e[32m[+] Active Subdomains saved to active_subs.txt\e[0m"
-echo -e "\e[30m[+] Redirect Subdomains saved to redirect_subs.txt\e[0m"
+echo -e "\e[34m[+] Redirect Subdomains saved to redirect_subs.txt\e[0m"
 echo -e "\e[31m[-] Forbidden Subdomains saved to forbidden_subs.txt\e[0m"
 echo -e "\e[33m[*] Takeover Subdomains saved to takeovers_subs.txt\e[0m"
